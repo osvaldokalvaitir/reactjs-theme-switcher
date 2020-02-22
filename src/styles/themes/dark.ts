1b3d24724f40d11708d22edff174e3815d2ec249
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-02-22 18:17:16 -0300

    Projeto concluído

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+  
+  colors: {
+    primary: '#333', 
+    secundary: '#A50104',
+
+    background: '#222',
+    text: '#fff',
+  }  
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+  
+  colors: {
+    primary: '#A50104', 
+    secundary: '#7C7C7C',
+
+    background: '#f5f5f5',
+    text: '#333',
+  }  
+};
